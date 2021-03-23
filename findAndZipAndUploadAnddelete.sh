5177363d142752c5c4245144cb1f87a9a5114bf0
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-03-23 19:26:03 +0800

    Update findAndZipAndUploadAnddelete.sh

--- findAndZipAndUploadAnddelete.sh
+++ findAndZipAndUploadAnddelete.sh
@@ -1,12 +1,13 @@
-
+for ((i=1; i<=14400; i++))
+do
     # 找到一个符合要求的文件
-    shouldBeUploaded=$(find . -type f -size +3M | grep -v '!qB$' | sed -n 1p)
-    echo currentFile-${shouldBeUploaded}
+    shouldBeUploaded=$(find . -type f -size +90M | grep -v '!qB$' | sed -n 1p)
+    echo "currentFile: ${shouldBeUploaded}"
     empty=""
     if [[ ${empty} = ${shouldBeUploaded} ]] ; then
         echo "empty! sleep 60s"
         sleep 60
-#        continue
+        continue
     fi
     # 获取文件所在路径
     dir=$(dirname "${shouldBeUploaded}")
@@ -16,12 +17,12 @@
     #加密压缩
     zip -P Xuan19981224 "${zipTmp}" "${shouldBeUploaded}"
     #分卷
-    zip -s 1m "${zipTmp}" --out "${zipFile}"
+    zip -s 18m "${zipTmp}" --out "${zipFile}"
     #删除源文件与临时文件
     rm -rf "${shouldBeUploaded}" "${zipTmp}"
     ############################################################################################
     #上传 zip 文件
-    output=`bypy -d --include-regex ".+\.z(\d+|ip)" syncup`
+    bypy -v --include-regex ".+\.z(\d+|ip)" syncup
     ############################################################################################
     
     bypyCompareInfo=bypyCompareInfo$(date +%s)${RANDOM}.txt
@@ -29,7 +30,7 @@
     
     bypy compare ${dir} ${dir} > ${bypyCompareInfo}
     
-    cat ${bypyCompareInfo}
+#    cat ${bypyCompareInfo}
     
     sameFilesAt=`cat ${bypyCompareInfo} | grep -n 'Same files' | cut -d ':' -f 1`
 #    echo ${sameFilesAt}
@@ -44,6 +45,7 @@
     done
     
     rm -rf ${bypyCompareInfo}
-    
-#    echo ${i}-${shouldBeUploaded}
-#    sleep 60
+
+    echo "${i}th file ${shouldBeUploaded} is complete! sleep 60s"
+    sleep 60
+done

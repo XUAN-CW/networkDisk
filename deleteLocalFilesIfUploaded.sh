99918e5d3462672e49b6f131e639a2db3b066327
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-03-23 10:31:32 +0800

    给变量换了个名字

--- deleteLocalFilesIfUploaded.sh
+++ deleteLocalFilesIfUploaded.sh
@@ -1,18 +1,18 @@
-tempFile=tempfile${RANDOM}.txt
+bypyCompareInfo=bypyCompareInfo${RANDOM}.txt
 
-bypy compare > ${tempFile}
+bypy compare > ${bypyCompareInfo}
 
-sameFilesAt=`cat ${tempFile} | grep -n 'Same files' | cut -d ':' -f 1`
+sameFilesAt=`cat ${bypyCompareInfo} | grep -n 'Same files' | cut -d ':' -f 1`
 #echo ${sameFilesAt}
-differentFilesAt=`cat ${tempFile} | grep -n 'Different files' | cut -d ':' -f 1`
+differentFilesAt=`cat ${bypyCompareInfo} | grep -n 'Different files' | cut -d ':' -f 1`
 #echo ${differentFilesAt}
 
 
-sameFilePathArray=($(cat ${tempFile} | sed -n "${sameFilesAt},${differentFilesAt}p" | grep '^F' | awk '{print $3}'))
+sameFilePathArray=($(cat ${bypyCompareInfo} | sed -n "${sameFilesAt},${differentFilesAt}p" | grep '^F' | awk '{print $3}'))
 for element in $(seq 0 $((${#sameFilePathArray[*]}-1)))
 do
     rm -rf ${sameFilePathArray[$element]}
 done
 
 
-rm -rf ${tempFile}
+rm -rf ${bypyCompareInfo}

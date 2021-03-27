4aa6329b1a01ec095252eee61e3ef8d4920967f9
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-03-28 01:20:17 +0800

    修复了路径名称有空格 awk 截取错误的错误

--- deleteSomething.sh
+++ deleteSomething.sh
@@ -2,7 +2,7 @@ for ((i=1; i<=14400; ))
 do
     echo $(date)
     #根据日志删除已经上传的文件
-    tail -n 3000 FindAndZipAndUpload.out | grep -a zipFile |grep -a "==>" |grep -a OK |awk '{print $1}' | xargs rm -rf
+    tail -n 3000 FindAndZipAndUpload.out | grep -a zipFile |grep -a "==>" |grep -a OK |  cut -d = -f 1 | xargs rm -rf
     #清空日志
 #    >FindAndZipAndUpload.out
     #删除空文件夹

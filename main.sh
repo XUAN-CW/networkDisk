7435407b6b7b2da8982e643c8acb5a1f47557450
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-04-20 08:56:40 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,3 +2,5 @@ chmod -R 777 .
 nohup ./findAndZipAndUpload.sh > FindAndZipAndUpload.out 2>&1 &
 nohup ./deleteSomething.sh > DeleteSomething.out 2>&1 &
 nohup ./contOrStopQbit.sh > ContOrStopQbit.out 2>&1 &
+
+nohup ./showInfo.sh > ShowInfo.html 2>&1 &

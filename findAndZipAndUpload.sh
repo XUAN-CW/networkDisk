bcf4153cf5c7f8ce109a414aa738d3dd844fde36
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-05-08 02:05:40 +0800

    Update findAndZipAndUpload.sh

--- findAndZipAndUpload.sh
+++ findAndZipAndUpload.sh
@@ -47,43 +47,23 @@ do
     dir="${dir}/"$(echo "${shouldBeUploaded##*/}"|sed $'s/[^[:alnum:]\/]/_/g')
     echo "dir:${dir}"
     mkdir  -p "${dir}"
-    mv "${shouldBeUploaded}" "${dir}/"
     ###############################################################
-    echo "设置压缩文件"
-    zipTmp="${dir}/smallFile_${zipFileFlag}_$(date +%s).zip"
-    bigFile="${dir}/bigFile_${zipFileFlag}_$(date +%s).zip"
     echo "加密压缩"
-    zip -rP Xuan19981224 "${dir}/" "${shouldBeUploaded}" -m
+    zip -rP Xuan19981224 "${dir}/${zipFileFlag}_$(date +%s).zip" "${shouldBeUploaded}" -m
     ###############################################################
     echo "bypy 上传到百度云"
-    bypy sync
-    isSameFile=false
-    #逐行读取
-    bypy compare "${dir}/" "${dir}/" | while read line
-    do
-        # 找到相同文件开始的位置
-        if [ "$line" = "==== Same files ===" ];then
-            isSameFile=true
-            continue
-        fi
-        
-        # 找到相同文件结束的位置
-        if [ "$line" = "==== Different files ===" ];then
-            break
-        fi
-        
-        # 处于相同文件的地方
-        if [ "$isSameFile" = "true" ];then
-            deletedFile=$(echo ${line} | sed  's/^....//g')
-            echo "删除了 ""${deletedFile}"
-            rm -rf "${deletedFile}"
-        fi
-    done
+    bypy syncup "${dir}/" "${dir}/" 
+    
+    ###############################################################
+    # 这里跟一个不存在的目录比较,跟不存在的目录比较后得到的就是所有的远程文件了
+    currentPath=`pwd`
+    cd "${dir}/"
+    bypy -v compare "${dir}/" inexistenceFile | grep '^F' | awk '{print $3}' | xargs -i rm -rf {}
+    cd "${currentPath}/"
+#    break
     ###############################################################
     echo "删除空文件夹"
     find -type d -empty | sed 's/\(.*\)/\"\1\"/' | xargs rmdir
-#    break
-    echo "complete! ${i}th file ${shouldBeUploaded} is uploaded!"
     ((i++))
 done
 
